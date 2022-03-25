1722dec50d44758de796cbadfa56ebaa2d9ebb40
Ruijian Cao <caoruijian@big.one>
2022-03-25 16:41:26 +0800

    config tailwind css

--- lab/phx_assets/config/dev.exs
+++ lab/phx_assets/config/dev.exs
@@ -25,7 +25,8 @@ config :phx_assets, PhxAssetsWeb.Endpoint,
   secret_key_base: "OYJJSU0O7M1kXgugZKFE0llqALfgM33KFGVtoWZhfWIVq5aHwKLnpfaSABEO0jKa",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
