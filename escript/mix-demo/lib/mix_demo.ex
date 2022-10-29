5a213e91d07e49dc072ad95a6365f769dcaaf091
Ruijian Cao <caoruijian@big.one>
2022-10-29 12:30:48 +0800

    fix

--- escript/mix-demo/lib/mix_demo.ex
+++ escript/mix-demo/lib/mix_demo.ex
@@ -18,9 +18,7 @@ defmodule MixDemo do
   end
 
   def main(args) do
-    {opts, positional_args, errors} =
-      args
-      |> parse_args
+    {opts, positional_args, errors} = args |> parse_args
 
     case errors do
       [] ->
@@ -34,15 +32,13 @@ defmodule MixDemo do
     end
   end
 
-  defp show_jason(args) do
-    {:ok, content} = Jason.encode(args)
-    IO.puts("json content: #{content}")
-  end
-
   defp parse_args(args) do
     {opts, cmd_and_args, errors} =
       args
-      |> OptionParser.parse(strict: [verbose: :boolean, count: :integer])
+      |> OptionParser.parse(
+        strict: [verbose: :boolean, count: :integer],
+        aliases: [c: :count]
+      )
 
     {opts, cmd_and_args, errors}
   end
@@ -51,11 +47,13 @@ defmodule MixDemo do
     count = Keyword.get(opts, :count, 1)
 
     printfn =
-      if not Keyword.has_key?(opts, :verbose) do
-        fn arg -> IO.puts(arg) end
-      else
+      if Keyword.has_key?(opts, :verbose) do
         fn arg ->
           IO.write("Message: ")
+          IO.puts(arg)
+        end
+      else
+        fn arg ->
           IO.puts(arg)
         end
       end
@@ -63,10 +61,14 @@ defmodule MixDemo do
     Stream.iterate(0, &(&1 + 1))
     |> Stream.take(count)
     |> Enum.each(fn _counter ->
-      Enum.with_index(args)
-      |> Enum.each(fn {arg, idx} ->
+      Enum.with_index(args, fn arg, idx ->
         printfn.("#{idx}. #{arg}")
       end)
     end)
+  end
+
+  defp show_jason(args) do
+    {:ok, content} = Jason.encode(args)
+    IO.puts("json content: #{content}")
   end
 end
